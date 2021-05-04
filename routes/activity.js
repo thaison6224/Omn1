495cb8d125aea3b2c0e71bcf2aae1708c07a2f0f
thaison <thaison113@hotmail.com>
2021-05-04 10:55:11 +0700

    update

--- routes/activity.js
+++ routes/activity.js
@@ -66,7 +66,8 @@ exports.edit = function (req, res) {
  */
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
+    console.log( 'exports.save' );
+    console.log( req.body );
     logData(req);
     fs.appendFile('execute_log.txt', "\r\n"+"execute", function (err) {
       if (err) throw err;
